a36b80568d9666301e88d473acd9988d41cbe47b
YaCyr <Yae.Prog22@outlook.com>
2023-06-22 13:26:28 +0200

    les couleurs de them et le footer

--- resources/views/includes/usage/footer-links.blade.php
+++ resources/views/includes/usage/footer-links.blade.php
@@ -1,4 +1,4 @@
-<div class="col-6 footer-links d-flex flex-row flex-wrap justify-content-around mt-4">
+{{-- <div class="col-6 footer-links d-flex flex-row flex-wrap justify-content-around mt-4">
  @foreach ($rubrics as $rubric)
   @can('view', $rubric)
    @if($rubric->hasChilds())
@@ -21,4 +21,29 @@
    @endif
   @endcan
  @endforeach
+</div> --}}
+
+<div class="col-6 footer-links d-flex flex-wrap justify-content-around mt-4">
+    @foreach ($rubrics as $rubric)
+     @can('view', $rubric)
+      @if($rubric->hasChilds())
+       <div class="d-flex flex-column">
+           <h4>{{ $rubric->name }}</h4>
+           <ul>
+             @foreach ($rubric->childs as $childRubric)
+               <li>
+                   <i class="bx bx-chevron-right"></i>
+                   <a href="/{{ $childRubric->segment }}">{{ $childRubric->name }}</a>
+               </li>
+             @endforeach
+           </ul>
+       </div>
+      @else
+        <h4>
+            <a href="/{{ $rubric->segment }}"
+            @if($rubric->segment == 'dashboard') target="_blank" @endif>{{ $rubric->name }}</a>
+        </h4>
+        @endif
+     @endcan
+    @endforeach
 </div>
