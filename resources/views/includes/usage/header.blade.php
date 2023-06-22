5abad3f5b518dcabd609dfee1b590e4d5be0e527
PierreLagodiuk <pierre.lagodiuk@citeformations.com>
2023-06-22 16:53:28 +0200

    retour header original

--- resources/views/includes/usage/header.blade.php
+++ resources/views/includes/usage/header.blade.php
@@ -1,7 +1,7 @@
 <header id="header" class="@if($fixedTop) fixed-top @endif header-inner-pages">
-    <div class="d-flex justify-content-between align-items-center mx-5">
-        <a href="/" class="logo my-1">
-            <img src="{{ asset('img/logo.jpg') }}"
+    <div class="container d-flex align-items-center">
+        <a href="/" class="col-1 me-auto">
+            <img src="{{ asset('img/logo.jpg') }}" class="img-fluid"
                  title="Info-Cité : L'intranet de la Cité des Formations"
                  alt="Info-Cité : L'intranet de la Cité des Formations - Tours">
         </a>
