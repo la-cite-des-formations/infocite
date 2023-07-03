f7a4af3b659a00b1cdbcec8f16c20c6710ae6eca
YaCyr <Yae.Prog22@outlook.com>
2023-07-03 09:09:24 +0200

    couleur de alert

--- resources/views/livewire/modals/usage/confirm.blade.php
+++ resources/views/livewire/modals/usage/confirm.blade.php
@@ -3,12 +3,12 @@
 @section('modal-title', "Confirmation")
 
 @section('modal-body')
-    <div class="alert alert-secondary mb-3">
+    <div class="alert alert-{{$handling === 'update' || $handling === 'create' ? 'success' : 'danger'}} mb-3">
         <p>{{$message}}</p>
     </div>
 @endsection
 
 @section('modal-footer')
     <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Annuler</button>
-    <button type="button" class="btn btn-danger" wire:click="confirm">OK</button>
+    <button type="button" class="btn btn-{{$handling === 'update' || $handling === 'create' ? 'primary' : 'danger'}}" wire:click="confirm">OK</button>
 @endsection
