16fdfb7808fd4dbef489608101a779a91cadf05f
PierreLagodiuk <pierre.lagodiuk@citeformations.com>
2023-08-28 10:46:44 +0200

    identity error correction

--- resources/views/includes/admin/processes/formTabs/general.blade.php
+++ resources/views/includes/admin/processes/formTabs/general.blade.php
@@ -62,7 +62,7 @@
         <select id="process-manager" wire:model="process.manager_id" type="input" class="col form-control">
             <option label="Choisir le responsable..."></option>
           @foreach($managers as $manager)
-            <option value='{{ $manager->id }}'>{{ $manager->identity() }}</option>
+            <option value='{{ $manager->id }}'>{{ $manager->identity }}</option>
           @endforeach
         </select>
     </div>
