f4b7baa0580031d93611012bb0b32fb1fd464abf
PierreLagodiuk <pierre.lagodiuk@citeformations.com>
2024-01-08 10:27:11 +0100

    add csp livewire styles

--- resources/views/layouts/admin-app.blade.php
+++ resources/views/layouts/admin-app.blade.php
@@ -2,7 +2,7 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}" style="height: 100%">
     <head>
         @include('includes.admin.head')
-        @livewireStyles
+        @livewireStyles(['nonce' => csp_nonce()])
     </head>
     <body class="h-100 bg-dark">
         <div id="app" class="h-100">
