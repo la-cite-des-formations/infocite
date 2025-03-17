11d0cc0505a6255f3a834bdb522d4074357a9a8d
PierreLagodiuk <pierre.lagodiuk@citeformations.com>
2025-03-17 13:54:03 +0100

    websockets removed

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -26,25 +26,25 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
  * allows your team to easily build robust real-time web applications.
  */
 
-import Echo from 'laravel-echo';
-import Pusher from 'pusher-js';
+// import Echo from 'laravel-echo';
+// import Pusher from 'pusher-js';
 
-window.Pusher = Pusher;
+// window.Pusher = Pusher;
 
-window.Echo = new Echo({
-    broadcaster: 'pusher',
-    key: process.env.MIX_PUSHER_APP_KEY,
-    cluster: process.env.MIX_PUSHER_APP_CLUSTER,
-    wsHost: process.env.MIX_WEBSOCKETS_HOST,
-    wsPort: process.env.MIX_WEBSOCKETS_PORT,
-    wssPort: process.env.MIX_WEBSOCKETS_PORT,
-    forceTLS: process.env.MIX_WEBSOCKETS_SCHEME === 'https',
-    enabledTransports: ['ws', 'wss'],
-    disableStats: true
-});
+// window.Echo = new Echo({
+//     broadcaster: 'pusher',
+//     key: process.env.MIX_PUSHER_APP_KEY,
+//     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
+//     wsHost: process.env.MIX_WEBSOCKETS_HOST,
+//     wsPort: process.env.MIX_WEBSOCKETS_PORT,
+//     wssPort: process.env.MIX_WEBSOCKETS_PORT,
+//     forceTLS: process.env.MIX_WEBSOCKETS_SCHEME === 'https',
+//     enabledTransports: ['ws', 'wss'],
+//     disableStats: true
+// });
 
-window.Echo.channel('test-channel')
-    .listen('.TestEvent', (e) => {
-        console.log('Événement reçu:', e);
-        alert(e.message); // Affiche le message de l'événement
-    });
+// window.Echo.channel('test-channel')
+//     .listen('.TestEvent', (e) => {
+//         console.log('Événement reçu:', e);
+//         alert(e.message); // Affiche le message de l'événement
+//     });
