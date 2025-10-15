2afba0a5b8801206448ad4f1731bf5aae19667b5
PierreLagodiuk <pierre.lagodiuk@citeformations.com>
2025-10-15 16:13:53 +0200

    daily scheduled fcm token purge

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,6 +26,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         $schedule->job(new PurgeInvalidFcmTokens())->daily();
+        $schedule->command('fcm:retain-latest-tokens --keep=5')->daily();
     }
 
     /**
